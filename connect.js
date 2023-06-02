cd9908c3da64387796933d049e8c282ddc14b8ee
tongsiripath <tong.siripath@gmail.com>
2023-06-02 16:45:52 +0700

    Edit Three

--- connect.js
+++ connect.js
@@ -1,8 +1,10 @@
-import mysql from "mysql"
+import mysql from "mysql";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "reacthosting_db"
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
